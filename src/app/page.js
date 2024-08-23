f0a7e2f377876e1dbeef90df9e10511d8b74f8aa
ShwetaDhumal06 <shwetamca2022_81@ncrdsims.edu.in>
2024-08-23 17:43:33 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-
-import Image from "next/image";
-import Home from "@/components/Home/Header";
-import Header from "@/components/Home/Header";
+"use client";
 import Testimonials from "@/components/Testinomal/Testinomals";
 import Navbar from "@/components/Home/Navbar";
 import Footer from "@/components/Home/Footer";
 import BlogSection from "@/components/Home/BlogSection";
+import Mid from "@/components/Home/Mid";
+import Hero from "@/components/Home/Hero";
 export default function Page() {
 return (
  <>
  <Navbar />
- <Header/>
+ <Hero />
+ <Mid />
  <Testimonials />
  <BlogSection />
  <Footer />
