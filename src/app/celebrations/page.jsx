c33c59a72159e2f88db5bbba31eb77f87a61051a
Siddhant <siddhant999123@gmail.com>
2024-10-01 17:19:43 +0530

    commit

--- src/app/celebrations/page.jsx
+++ src/app/celebrations/page.jsx
@@ -1,14 +1,14 @@
-import Celebrations from '@/components/celebrations/Celebrations'
-import Footer from '@/components/Home/Footer'
-import Footer from '@/components/Home/Footer'
-import Navbar from '@/components/Home/Navbar'
-import React from 'react'
+import Celebrations from "@/components/celebrations/Celebrations";
+import Footer from "@/components/Home/Footer";
+import Navbar from "@/components/Home/Navbar";
+import React from "react";
 
 export default function page() {
   return (
-    <div><Navbar/>
-   <Celebrations/>
-   <Footer/>
+    <div>
+      <Navbar />
+      <Celebrations />
+      <Footer />
     </div>
-  )
+  );
 }
